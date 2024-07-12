a3a7f78ab5ecce07f5493f41d24334f5f953431c
surafel meles <melessurafel413@gmail.com>
2024-07-12 22:13:42 +0300

    rps

--- script/script.js
+++ script/script.js
@@ -1,23 +1,42 @@
 let userInput;
 
 
+
+
 function RPSGame(userInput) {
+
+    let score = JSON.parse(localStorage.getItem('score'))
     let resalt;
     const randomNum = Math.round(Math.random() * 2);
     const Choice = ['rock', 'paper', 'scissor'];
     const randomChoice = Choice[randomNum];
     if (userInput === randomChoice) {
-        resalt = 'Tie';
+        resalt = 'tie';
+        score.tie += 1;
     } else if (userInput === 'rock' && randomChoice === 'scissor' ||
         userInput === 'scissor' && randomChoice === 'paper' ||
         userInput === 'paper' && randomChoice === 'rock'
     ) {
-        resalt = 'Win'
+        resalt = 'you win';
+        score.win += 1;
     } else if (randomChoice === 'rock' && userInput === 'scissor' ||
         randomChoice === 'scissor' && userInput === 'paper' ||
         randomChoice === 'paper' && userInput === 'rock'
     ) {
-        resalt = 'loss'
+        resalt = 'you loss';
+        score.loss += 1;
     }
-    alert(resalt)
+
+    if (userInput === "reset score") {
+        score.loss = 0;
+        score.tie = 0;
+        score.win = 0;
+        resalt = 'you reset score';
+    }
+    localStorage.setItem('score', JSON.stringify(score))
+
+    document.querySelector('.first').innerHTML = resalt
+    document.querySelector('.second').innerHTML = `you pick ${userInput},  computer pick ${randomChoice}.`;
+    document.querySelector('.third').innerHTML = `Win: ${score.win}, Loss: ${score.loss}, Tie: ${score.tie}`;
+
 }
